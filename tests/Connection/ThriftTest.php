7fe8dc25cdb7c2d32648f437720fccdee428e175
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-01-09 01:54:57 +0100

    fix test

--- tests/Connection/ThriftTest.php
+++ tests/Connection/ThriftTest.php
@@ -74,7 +74,7 @@ class ThriftTest extends TestCase
 
         $this->assertInstanceOf(Container::class, $builder->getForm());
 
-        $elements = $builder->getForm()->getProperty('element');
+        $elements = $builder->getForm()->getElements();
         $this->assertEquals(7, count($elements));
         $this->assertInstanceOf(Input::class, $elements[0]);
         $this->assertInstanceOf(Input::class, $elements[1]);
